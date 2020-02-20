477a5adc2156b18b8190f3acb4cde6e60ff557d5
Devin <devin.m.mcafee-1@ou.edu>
2020-02-20 16:02:58 -0600

    Equals test - not complete

---
+++ src/CarTest2.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CarTest2 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testCar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsCar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
